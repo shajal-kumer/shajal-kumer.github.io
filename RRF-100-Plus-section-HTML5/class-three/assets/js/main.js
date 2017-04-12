5abff165c987909a481b78c95ccf1fd05b9fe080
shajal-kumer <shajalkumer@gmail.com>
2017-04-13 01:23:01 +0600

    Class Three

---
+++ RRF-100-Plus-section-HTML5/class-three/assets/js/main.js
@@ -0,0 +1,12 @@
+(function($) {
+    'use strict';
+    
+    jQuery(document).ready(function() {
+        
+        $(".video-play-btn, .video-play-btn3").magnificPopup({
+        	type: 'iframe'
+        });
+        
+        
+    });// End of jQuery document function   
+}(jQuery));
