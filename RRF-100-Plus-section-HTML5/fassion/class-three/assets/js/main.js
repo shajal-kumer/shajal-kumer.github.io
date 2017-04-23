9fd75dc425979006ba4640245952a259068edb08
shajal-kumer <shajalkumer@gmail.com>
2017-04-23 23:17:30 +0600

    fassion

---
+++ RRF-100-Plus-section-HTML5/fassion/class-three/assets/js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+    'use strict';
+    
+    jQuery(document).ready(function() {
+        
+
+        $(".staff-list").owlCarousel({
+        	item: 4,
+        	autoplay: false,
+        })
+        
+    });// End of jQuery document function   
+}(jQuery));
