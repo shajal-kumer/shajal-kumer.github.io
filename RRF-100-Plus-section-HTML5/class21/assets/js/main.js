2b398f58d47e3d48972cf9ee18139aad0485f6a5
shajal-kumer <shajalkumer@gmail.com>
2017-04-27 16:31:28 +0600

    class21

---
+++ RRF-100-Plus-section-HTML5/class21/assets/js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+    'use strict';
+    
+    jQuery(document).ready(function() {
+        
+
+        $(".homepage-slides").owlCarousel({
+        	items: 1,
+        	autoplay: false,
+        	loop: true,
+        	nav: false,
+        	dots: true
+        })
+        
+    });// End of jQuery document function   
+}(jQuery));
