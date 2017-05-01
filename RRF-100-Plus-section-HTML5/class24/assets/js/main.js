eb454fbb672c1a841f3f13296d155a2749af706a
shajal-kumer <shajalkumer@gmail.com>
2017-05-01 09:12:10 +0600

    js change

--- RRF-100-Plus-section-HTML5/class24/assets/js/main.js
+++ RRF-100-Plus-section-HTML5/class24/assets/js/main.js
@@ -21,12 +21,14 @@
 			$(".single-slide-item h2").removeClass("animated fadeInLeft").css({"-webkit-animation-delay":".5s", "opacity":"0"});
 			$(".single-slide-item p").removeClass("animated fadeInLeft").css({"-webkit-animation-delay":".8s", "opacity": "0"});
 			$(".single-slide-item .slide-readmore-btn").removeClass("animated fadeInLeft").css({"-webkit-animation-delay":"1.2s", "opacity":"0"});
+                        $(".single-slide-item .slide-img-right").removeClass("animated fadeInRight").css({"-webkit-animation-delay":".2s", "opacity":"0"});
 		});
 
 		$(".homepage-slides").on("translated.owl.carousel", function() {
 			$(".single-slide-item h2").addClass("animated fadeInLeft").css({"-webkit-animation-delay":".5s", "opacity":"1"});
 			$(".single-slide-item p").addClass("animated fadeInLeft").css({"-webkit-animation-delay": ".8s", "opacity": "1"});
 			$(".single-slide-item .slide-readmore-btn").addClass("animated fadeInLeft").css({"-webkit-animation-delay":"1.2s", "opacity":"1"});
+            $(".single-slide-item .slide-img-right").addClass("animated fadeInRight").css({"-webkit-animation-delay":".2s", "opacity":"1"});
 		});
 
         
