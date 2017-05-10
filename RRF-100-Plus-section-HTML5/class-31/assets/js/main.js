7638bc1ce9564d9dbd9b2dc64b4e1519d787f590
shajal-kumer <shajalkumer@gmail.com>
2017-05-10 13:13:28 +0600

    31

---
+++ RRF-100-Plus-section-HTML5/class-31/assets/js/main.js
@@ -0,0 +1,10 @@
+(function($) {
+    'use strict';
+    
+    jQuery(document).ready(function() {
+        
+
+        var s = skrollr.init();
+        
+    });// End of jQuery document function   
+}(jQuery));
