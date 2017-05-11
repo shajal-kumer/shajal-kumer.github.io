f9633d23e1c4b75cc58c70261d1f90754d1f6945
shajal-kumer <shajalkumer@gmail.com>
2017-05-11 10:58:19 +0600

    32

---
+++ RRF-100-Plus-section-HTML5/class-32/assets/js/main.js
@@ -0,0 +1,22 @@
+(function($) {
+    'use strict';
+    
+    jQuery(document).ready(function() {
+        
+
+        $(window).scroll(function() {
+        	if($(this).scrollTop() > 100) {
+        		$('.scrollToTop').fadeIn();
+        	} else {
+        		$('.scrollToTop').fadeOut();
+        	}
+        });
+
+
+        $('.scrollToTop').on('click', function() {
+        	$('html, body').animate({scrollTop: 0}, 800);
+        	return false;
+        });
+        
+    });// End of jQuery document function   
+}(jQuery));
