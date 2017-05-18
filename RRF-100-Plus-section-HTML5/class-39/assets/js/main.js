1e484ee072cb90bd253e6651bb159196f3e26a77
shajal-kumer <shajalkumer@gmail.com>
2017-05-18 23:40:37 +0600

    39 off cancas menu

--- RRF-100-Plus-section-HTML5/class-39/assets/js/main.js
+++ RRF-100-Plus-section-HTML5/class-39/assets/js/main.js
@@ -3,6 +3,19 @@
     
     jQuery(document).ready(function() {
         
+    	$('.menu-triger').on('click', function() {
+    		$('.off-canvas-menu, .off-canvas-overlay').addClass('active');
+    	})
+    	$('.menu-close').on('click', function() {
+    		$('.off-canvas-menu, .off-canvas-overlay').removeClass('active');
+    	})
+
+    	$('.off-canvas-overlay').on('click', function() {
+    		$(this).removeClass('active');
+    	})
+    	$('.off-canvas-overlay').on('click', function() {
+    		$('.off-canvas-menu').removeClass('active');
+    	})
 
         
     });// End of jQuery document function   
