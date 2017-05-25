655112a030d08f1883330cdaff685652fad8c409
shajal-kumer <shajalkumer@gmail.com>
2017-05-26 01:46:22 +0600

    classs 46

---
+++ RRF-100-Plus-section-HTML5/class-46/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $('.featured-trailor-carousel').owlCarousel({
+       	items: 1,
+       	autuplay: true,
+       	autuplaytime:1500,
+       	loop:true,
+       	nav: false,
+       	dots: true,
+       	smartspeed:800
+       })
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
