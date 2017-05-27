8a36e4c295e41eab4e1e8c4bbb9bc944fc1a0a34
shajal-kumer <shajalkumer@gmail.com>
2017-05-27 07:21:57 +0600

    class 47

---
+++ RRF-100-Plus-section-HTML5/class-47/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $('.testimonial-carousel').owlCarousel({
+       	items: 1,
+       	loop: true,
+        dots: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 800
+       })
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
