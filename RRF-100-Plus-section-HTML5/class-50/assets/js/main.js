f9c802d5fa6f4daafe3816db45543dbcf0eeed1d
shajal-kumer <shajalkumer@gmail.com>
2017-06-04 13:25:56 +0600

    class-50

---
+++ RRF-100-Plus-section-HTML5/class-50/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".listing-carousel").owlCarousel({
+       	items: 3,
+       	loop: true,
+       	dots:true,
+       	nav:false,
+       	autoplay: false,
+       	margin:30
+       })
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
