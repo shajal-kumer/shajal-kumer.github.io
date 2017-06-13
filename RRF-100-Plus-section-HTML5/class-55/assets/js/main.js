2f06d1313ce3b2ecf1d95a260e3c776db39c5f78
shajal-kumer <shajalkumer@gmail.com>
2017-06-13 21:35:40 +0600

    class-55

---
+++ RRF-100-Plus-section-HTML5/class-55/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".video-play-btn").magnificPopup({
+        	type: 'iframe'
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
