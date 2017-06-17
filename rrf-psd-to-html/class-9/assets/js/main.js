8db941b69e74d4c643435aacfb452868e08a3a14
shajal-kumer <shajalkumer@gmail.com>
2017-06-17 23:32:01 +0600

    class-9

---
+++ rrf-psd-to-html/class-9/assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $('.video-pay-btn').magnificPopup({
+            type: 'iframe'
+       })
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
