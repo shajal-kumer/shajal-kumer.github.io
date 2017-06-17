3d4b2e89dd43020b075dfea16da62c6271e4ddd9
shajal-kumer <shajalkumer@gmail.com>
2017-06-17 12:44:52 +0600

    class-7

---
+++ RRF-100-Plus-section-HTML5/class-57/assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+
+    	$('.testimonial-author-name').perfectScrollbar({
+    		// Default: true,
+    		// Default: 3
+    	});  
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
