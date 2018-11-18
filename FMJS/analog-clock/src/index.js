2d84370bad45e60096a25d54cc66a083d6c77bb8
shajal-kumer <shajalkumer@gmail.com>
2018-11-18 23:37:53 +0600

    chat

--- FMJS/analog-clock/src/index.js
+++ FMJS/analog-clock/src/index.js
@@ -3,7 +3,6 @@ let minute = document.querySelector(".minute");
 let second = document.querySelector(".second");
 
 function setTime(element, degree) {
-  degree -= 90;
   element.style.transform = `rotateZ(${degree}deg)`;
 }
 
@@ -13,10 +12,17 @@ function tick() {
   let m = d.getMinutes();
   let s = d.getSeconds();
 
-  // set hours
-  setTime(hour, h * 30);
-  setTime(minute, m * 6);
   setTime(second, s * 6);
+  let secondsDegrees = (s / 60) * 360 - 90;
+  setTime(second, secondsDegrees);
+  console.log(secondsDegrees);
+
+  setTime(minute, m * 6);
+  let minsDegrees = (m / 60) * 360 + (s / 60) * 6 - 90;
+  setTime(minute, minsDegrees);
+
+  let hourDegree = (h / 12) * 360 + (m / 60) * 30 - 90;
+  setTime(hour, hourDegree);
 }
 
 setInterval(tick, 1000);
