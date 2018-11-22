0165397ffdfd8703d94d7a6316bae3d52a7a9ed6
shajal-kumer <shajalkumer@gmail.com>
2018-11-22 20:05:15 +0600

    Buyer work my choiche

---
+++ bw/client-my-choice/js/main.js
@@ -0,0 +1,24 @@
+(function($) {
+  "use strict";
+
+  jQuery(document).ready(function($) {
+    $("#Elchat").ChatSocket({
+      lblEntradaNombre: "Enter a user name in the field below for the chat room"
+    });
+
+    $(".chat__box--btn").on("click", function() {
+      $(".chat__box-field").toggleClass("open");
+      if ($(".chat__box-field").hasClass("open")) {
+        $(this).removeClass("active");
+      }
+    });
+
+    $("#txtMensaje").on("click", function() {
+      console.log("Hello");
+
+      $(".chat__box--btn").removeClass("active");
+    });
+  });
+
+  jQuery(window).load(function() {});
+})(jQuery);
