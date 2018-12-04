fc21886a419c41fe44e38aee190cee9761c81ae2
shajalkumer@gmail.com <shajalkumer@gmail.com>
2018-12-04 15:41:46 +0600

    Alert

--- bw/rtc3-demo/js/main.js
+++ bw/rtc3-demo/js/main.js
@@ -1,1 +1,10 @@
-(function() {})();
+(function() {
+  setTimeout(() => {
+    var g = $("media-container").hasClass("other-video-holder");
+    if (g) {
+      console.log("Hello");
+    }
+  }, 5000);
+
+  //   console.log(g);
+})();
