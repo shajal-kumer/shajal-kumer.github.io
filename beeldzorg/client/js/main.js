0904033e5ece944a62dd19047a52e59da0bc83a6
shajalkumer@gmail.com <shajalkumer@gmail.com>
2018-12-23 23:20:14 +0600

    blezzzing

---
+++ beeldzorg/client/js/main.js
@@ -0,0 +1,18 @@
+(function($) {
+  "use strict";
+
+  jQuery(document).ready(function($) {
+    $(".chat__box--btn").on("click", function() {
+      $(".chat__box-field").toggleClass("open");
+      if ($(".chat__box-field").hasClass("open")) {
+        $(this).removeClass("active");
+      }
+    });
+
+    $("#txtMensaje").on("click", function() {
+      $(".chat__box--btn").removeClass("active");
+    });
+  });
+
+  jQuery(window).load(function() {});
+})(jQuery);
