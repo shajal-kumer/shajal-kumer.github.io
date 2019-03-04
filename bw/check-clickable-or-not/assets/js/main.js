5aed61c2721a4a7eff8e49243bee957db006ee55
Shajal Kumer <shajalkumer@gmail.com>
2019-03-05 01:25:01 +0600

    fg

---
+++ bw/check-clickable-or-not/assets/js/main.js
@@ -0,0 +1,11 @@
+(function($) {
+	"use strict";
+
+	jQuery(document).ready(function($) {
+		$(".fa ").on("click", function() {
+			$("body").css("background", "red");
+		});
+	});
+
+	jQuery(window).load(function() {});
+})(jQuery);
